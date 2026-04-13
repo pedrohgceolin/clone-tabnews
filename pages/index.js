eed4fc7180ca78a433b953863c760060b3aba2e6
Pedro Henrique Gonçalves Ceolin <57969215+pedrohgceolin@users.noreply.github.com>
2026-04-13 23:29:48 +0000

    adiciona scipts ´lint:check´  e ´lint:fix´

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1>Hello World us guri de POA.</h1>
+function Home() {
+  return <h1>Hello World us guri de POA.</h1>;
+}
+
+function teste() {
+  console.log("teste");
 }
 
 export default Home;
