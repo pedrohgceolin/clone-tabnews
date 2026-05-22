95ce2f834d3f9dfb197677c6705a8be1a542e5bc
Pedro Henrique Gonçalves Ceolin <57969215+pedrohgceolin@users.noreply.github.com>
2026-05-22 22:39:32 +0000

    status and get.test modified

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,23 @@
 import database from "infra/database.js";
+import { Query } from "pg";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result);
-  response.status(200).json({ chave: "ch" });
+  const updatedAt = new Date().toISOString();
+  const postgresVersion = await database.query("SHOW server_version;");
+
+  const maxConnections = await database.query(
+    "SELECT setting AS max_conexoes FROM pg_settings WHERE name = 'max_connections';",
+  );
+  const usedConnections = await database.query(
+    "SELECT count(*) FROM pg_stat_activity;",
+  );
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    postgres_version: postgresVersion.rows[0].server_version,
+    max_connections: parseInt(maxConnections.rows[0].max_conexoes),
+    used_connections: parseInt(usedConnections.rows[0].count),
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,18 @@
 test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status")
+  const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
-})
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+  const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parseUpdatedAt);
+
+  expect(responseBody.postgres_version).toBeDefined();
+  console.log(responseBody.postgres_version);
+
+  expect(responseBody.max_connections).toBeDefined();
+  console.log(responseBody.max_connections);
+
+  expect(responseBody.used_connections).toBeDefined();
+  console.log(responseBody.used_connections);
+});
