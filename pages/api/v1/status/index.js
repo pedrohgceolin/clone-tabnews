e63e6ea5c486b7509516675816c78acc83d9a08a
Pedro Henrique Gonçalves Ceolin <57969215+pedrohgceolin@users.noreply.github.com>
2026-06-26 21:46:42 +0000

    feat: create and use InternalServerError in api/v1/status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,25 +1,35 @@
 import database from "infra/database.js";
+import { InternalServerError } from "infra/errors.js";
 
 async function status(request, response) {
-  const updatedAt = new Date().toISOString();
-  const postgresVersion = await database.query("SHOW server_version;");
+  try {
+    const updatedAt = new Date().toISOString();
+    const postgresVersion = await database.query("SHOW server_version;");
 
-  const maxConnections = await database.query(
-    "SELECT setting AS max_conexoes FROM pg_settings WHERE name = 'max_connections';",
-  );
+    const maxConnections = await database.query(
+      "SELECT setting AS max_conexoes FROM pg_settings WHERE name = 'max_connections';",
+    );
 
-  const databaseName = process.env.POSTGRES_DB;
-  const usedConnections = await database.query({
-    text: "SELECT count(*) FROM pg_stat_activity WHERE datname = $1;",
-    values: [databaseName],
-  });
+    const databaseName = process.env.POSTGRES_DB;
+    const usedConnections = await database.query({
+      text: "SELECT count(*) FROM pg_stat_activity WHERE datname = $1;",
+      values: [databaseName],
+    });
 
-  response.status(200).json({
-    updated_at: updatedAt,
-    postgres_version: postgresVersion.rows[0].server_version,
-    max_connections: parseInt(maxConnections.rows[0].max_conexoes),
-    used_connections: parseInt(usedConnections.rows[0].count),
-  });
+    response.status(200).json({
+      updated_at: updatedAt,
+      postgres_version: postgresVersion.rows[0].server_version,
+      max_connections: parseInt(maxConnections.rows[0].max_conexoes),
+      used_connections: parseInt(usedConnections.rows[0].count),
+    });
+  } catch (error) {
+    console.log("Erro dentro do catch do controller");
+    const publicErrorObject = new InternalServerError({
+      cause: error,
+    });
+    console.log(publicErrorObject);
+    response.status(500).json({ publicErrorObject });
+  }
 }
 
 export default status;
