c2fe2af10e07f848b97c0791afebc1669d9459a3
Pedro Henrique Goncalves Ceolin <pedroceolin@Pedros-MacBook-Air.local>
2026-07-25 12:31:09 -0400

    feat: add creation of sessions feature

--- infra/controllers.js
+++ infra/controllers.js
@@ -3,6 +3,7 @@ import {
   MethodNotAllowedError,
   ValidationError,
   NotFoundError,
+  UnauthorizedError,
 } from "infra/errors.js";
 
 function onNoMatchhandler(request, response) {
@@ -11,12 +12,15 @@ function onNoMatchhandler(request, response) {
 }
 
 function onErrorHandler(error, request, response) {
-  if (error instanceof ValidationError || error instanceof NotFoundError) {
+  if (
+    error instanceof ValidationError ||
+    error instanceof NotFoundError ||
+    error instanceof UnauthorizedError
+  ) {
     return response.status(error.statusCode).json(error);
   }
 
   const publicErrorObject = new InternalServerError({
-    statusCode: error.statusCode,
     cause: error,
   });
 
